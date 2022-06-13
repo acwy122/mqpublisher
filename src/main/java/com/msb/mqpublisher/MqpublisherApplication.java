58ffe76bda491fdcbe085980f3846898628e931d
xurb <xurongbiao@hmtim.com>
2022-06-13 23:14:34 +0800

    mq  rabbitmq

---
+++ src/main/java/com/msb/mqpublisher/MqpublisherApplication.java
@@ -0,0 +1,13 @@
+package com.msb.mqpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqpublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqpublisherApplication.class, args);
+    }
+
+}
